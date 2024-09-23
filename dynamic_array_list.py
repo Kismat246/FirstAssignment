cc945d36e1e040ebb399a7dd59a631781d29c85a
Kismat Gurung <kismatgurung@Kismats-MacBook-Air.local>
2024-09-23 17:10:40 -0400

    resize function added

---
+++ dynamic_array_list.py
@@ -0,0 +1,14 @@
+class DynamicArrayList:
+
+    def __init__(self, initial_size=2):
+        self.array = [None, None]
+        self.size = 0
+        self.size_limit = initial_size
+
+    def __str__(self):
+        return f"{self.array} {self.size} {self.size_limit}"
+    
+    def resize():
+        print('resize')
+
+dynamic_array_list = DynamicArrayList()
